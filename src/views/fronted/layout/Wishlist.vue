ef443e600a289498c53e1592156c7cbc307361b1
Miko <miko82290@gmail.com>
2020-11-05 17:16:51 +0800

    create wish list

--- src/views/fronted/layout/Wishlist.vue
+++ src/views/fronted/layout/Wishlist.vue
@@ -1,11 +1,20 @@
 <template>
-  <div class="products">
-    <div v-for="item in wishlist" :key="item.id" class="item">
-      <div class="item-header">
-        <router-link :to="`/product/${item.id}`">
-          <img class="df" :src="`${item.imageUrl[0]}`" alt="">
-          <button type="button" class="view_btn">Quick View</button>
-        </router-link>
+  <div class="container df fxd-c">
+    <h3 class="wishTitle">Wish List</h3>
+    <div class="wishlist df jc-c ai-c fxw-w">
+      <div v-for="item in wishlist" :key="item.id" class="item">
+        <div class="item-header">
+          <router-link :to="`/product/${item.id}`">
+            <img class="df" :src="`${item.imageUrl[0]}`" alt="">
+            <button type="button" class="view_btn">Quick View</button>
+          </router-link>
+        </div>
+        <div class="item-body ta-c">
+          <router-link :to="`/product/${item.id}`">
+            <h4>{{item.title}}</h4>
+          </router-link>
+          <p>NT.{{item.price}}</p>
+        </div>
       </div>
     </div>
   </div>
